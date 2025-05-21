7548f76ab08c8e0981a5192165db070f666d7fae
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-05-21 17:27:08 +0300

    - При рестарте каждые ~6ч будет отписывать в лог канал об завершении работы

--- tasks/shutdown_timer.py
+++ tasks/shutdown_timer.py
@@ -1,9 +1,11 @@
 import asyncio
+from datetime import datetime
 
 from disnake.ext import tasks
 
 from bot_init import bot
-from config import SHUTDOWN_TIMER
+from config import LOG_TECH_CHANNEL, SHUTDOWN_TIMER
+from events.utils import get_guild_status
 from tasks.utils import save_private_channels
 
 
@@ -11,4 +13,29 @@ from tasks.utils import save_private_channels
 async def shutdown_timer():
     await asyncio.sleep(SHUTDOWN_TIMER)
     await save_private_channels()
+    
+    
+    # Получаем канал для логов
+    log_channel = bot.get_channel(LOG_TECH_CHANNEL)
+    if log_channel:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        status = await get_guild_status()
+        
+        ascii_art = f"""
+```diff
++-----------------------------------------------------+
+| SCPINET BOTT {current_time}                    |
+|                                                     |
+-->> SYSTEM ⚠ WARNING                                 |
+-->> RESTART IN 10 MINUTES                            |
+|                                                     |
+{status}           |
+|                                                     |
+-->> BOT SHUTDOWN COMPLETE                            |
+-->> GOODBYE.                                         |
++-----------------------------------------------------+
+```
+"""
+        await log_channel.send(ascii_art)
+
     await bot.close()
