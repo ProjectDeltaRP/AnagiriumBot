7c0ba3b32a1226de13d741aac0bea7cb48c060a1
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-05-23 01:30:38 +0300

    - add /ping command with slash

--- commands/general_commands.py
+++ commands/general_commands.py
@@ -2,6 +2,8 @@
 Модуль предназначенный для общих комманд бота
 """
 
+from disnake import AppCommandInteraction
+
 from bot_init import bot
 
 
@@ -10,6 +12,17 @@ async def ping(ctx):
     """
     Команда для проверки задержки бота.
     """
+
     latency = round(bot.latency * 1000)
     emoji = "🏓" if latency < 100 else "🐢"
     await ctx.send(f"{emoji} Pong! Задержка: **{latency}ms**")
+
+@bot.slash_command(name="ping", help="Проверяет задержку бота.")
+async def ping_command(interaction: AppCommandInteraction):
+    """
+    Команда для проверки задержки бота.
+    """
+
+    latency = round(bot.latency * 1000)
+    emoji = "🏓" if latency < 100 else "🐢"
+    await interaction.response.send_message(f"{emoji} Pong! Задержка: **{latency}ms**")
