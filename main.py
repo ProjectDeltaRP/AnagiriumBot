80ade212b0d064cd00fd6a35cb4500dbddeced26
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-05-26 00:00:56 +0300

    - Рефактор main.py. Введён метод автоматического импорта всех модулей во всей директории.

--- main.py
+++ main.py
@@ -2,13 +2,30 @@
 Модуль для запуска бота.
 """
 
+import importlib
+import pkgutil
+
 from bot_init import bot
-from commands import general_commands, voice_commands
 from config import DISCORD_TOKEN
-from events import on_ready, on_voice, on_slash_command
-from tasks import shutdown_timer
+
+
+def auto_import_package(package_name: str):
+    """Автоматически импортирует все модули внутри пакета"""
+    package = importlib.import_module(package_name)
+    for _, module_name, is_pkg in pkgutil.iter_modules(package.__path__):
+        importlib.import_module(f"{package_name}.{module_name}")
+
 
 if __name__ == "__main__":
+    # Автоматический импорт подмодулей
+    auto_import_package("commands")
+    auto_import_package("events")
+    auto_import_package("modules")
+    auto_import_package("tasks")
+    # ...
+    # Не забываем подключать
+    # новые директории с модуля
+
     if DISCORD_TOKEN == "NULL":
         print("[ERROR] Not DISCORD_KEY. Programm Dev-bot shutdown!!")
     else:
